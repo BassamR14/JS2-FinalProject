65f06ed6b689492f7b455bd0820812fb5ea06262
BassamR14 <bassam.ruwaida@gmail.com>
2026-02-09 15:50:59 +0100

    Finished Timers + styling

--- index.js
+++ index.js
@@ -33,34 +33,11 @@ class Tamagotchi {
     this.fullness = Math.max(0, this.fullness - 10);
     this.happiness = Math.max(0, this.happiness - 10);
 
-    //if any value reaches 0 due to decay
+    //Check if any value reaches 0 due to decay
     const leavingTamas = Game.checkTamagotchis();
 
     if (leavingTamas.length > 0) {
-      const activityContainer = document.querySelector(".activity-container");
-      const activities = document.querySelector(".activities");
-      activityContainer.style.display = "block";
-
-      leavingTamas.forEach((tama) => {
-        //stop the instance's timer
-        clearInterval(tama.timer);
-
-        let reasons = [];
-        if (tama.energy === 0) {
-          reasons.push("energy");
-        }
-        if (tama.fullness === 0) {
-          reasons.push("fullness");
-        }
-        if (tama.happiness === 0) {
-          reasons.push("happiness");
-        }
-
-        let leavingMsg = document.createElement("p");
-        leavingMsg.classList.add("leaving-msg");
-        leavingMsg.innerText = `${tama.name} has left because their ${reasons.join(" & ")} reached 0`;
-        activities.prepend(leavingMsg);
-      });
+      Game.handleLeavingTamas(leavingTamas);
     }
 
     Game.render();
@@ -69,35 +46,31 @@ class Tamagotchi {
   //Since every tamagotchi has a timer, it belongs to the tamagochi class/instance
   startTimer() {
     let sec = 10;
-    this.timerDisplay.innerText = `${sec}s`;
 
-    this.timer = setInterval(() => {
-      sec--;
-      this.timerDisplay.innerText = `${sec}s`;
+    const tick = () => {
+      this.timerDisplay.innerText = sec < 10 ? `00:0${sec}s` : `00:${sec}s`;
 
+      // decay & reset + stop the function
       if (sec === 0) {
-        // run decay
         this.decay();
-        // reset countdown
         sec = 10;
+        return;
       }
-    }, 1000);
+
+      sec--;
+    };
+
+    //run the function then after one second, run again
+    tick();
+    this.timer = setInterval(tick, 1000);
+
+    //bind(this) is needed since setInterval doesn't know what this is, it becomes undefined.
+    // this.timer = setInterval(this.decay.bind(this), 10000);
   }
 }
 
 class Game {
   static tamagotchis = [];
-
-  static async getData() {
-    try {
-      let response = await fetch("https://randomuser.me/api");
-      let json = await response.json();
-      return json;
-    } catch (err) {
-      console.error("Failed to fetch user data:", err);
-      throw err;
-    }
-  }
 
   static async generateTamagotchi() {
     try {
@@ -107,14 +80,15 @@ class Game {
         return;
       }
 
-      let data = await Game.getData();
+      let response = await fetch("https://randomuser.me/api");
+      let data = await response.json();
 
       //To get a random animal
-      let animals = ["Tiger", "Wolf", "Dragon", "Phoenix"];
-      let index = Math.floor(Math.random() * animals.length);
+      const animals = ["Tiger", "Wolf", "Dragon", "Phoenix"];
+      const animalIndex = Math.floor(Math.random() * animals.length);
 
       const name = `${data.results[0].name.first} ${data.results[0].name.last}`;
-      const animalType = animals[index];
+      const animalType = animals[animalIndex];
       const energy = 50;
       const fullness = 50;
       const happiness = 50;
@@ -154,7 +128,13 @@ class Game {
   //this code is used multiple time for the buttons, if there are any tamagotchies with any value of 0, add activity message
   static handleLeavingTamas(leavingTamas) {
     const activities = document.querySelector(".activities");
+    const activityContainer = document.querySelector(".activity-container");
+    activityContainer.style.display = "block";
+
     leavingTamas.forEach((tama) => {
+      //stop the instance's timer
+      clearInterval(tama.timer);
+
       let reasons = [];
       if (tama.energy === 0) {
         reasons.push("energy");
@@ -175,7 +155,6 @@ class Game {
 
   static render() {
     const container = document.querySelector(".container");
-    const activityContainer = document.querySelector(".activity-container");
     const activities = document.querySelector(".activities");
     container.innerHTML = "";
 
@@ -206,15 +185,14 @@ class Game {
       happiness.innerText = `Happiness: ${tamagotchi.happiness}/100`;
 
       //choose the image based on animal type
-      if (tamagotchi.animalType === "Tiger") {
-        animalImage.src = "tiger.jpg";
-      } else if (tamagotchi.animalType === "Wolf") {
-        animalImage.src = "wolf.jpg";
-      } else if (tamagotchi.animalType === "Dragon") {
-        animalImage.src = "dragon.jpg";
-      } else if (tamagotchi.animalType === "Phoenix") {
-        animalImage.src = "phoenix.jpg";
-      }
+      const animalImages = {
+        Tiger: "tiger.jpg",
+        Wolf: "wolf.jpg",
+        Dragon: "dragon.jpg",
+        Phoenix: "phoenix.jpg",
+      };
+
+      animalImage.src = animalImages[tamagotchi.animalType];
 
       //Create buttons + add message of activity + check if any value reaches 0 after button press
       const btnDiv = document.createElement("div");
@@ -236,7 +214,6 @@ class Game {
         const leavingTamas = Game.checkTamagotchis();
         Game.render();
 
-        activityContainer.style.display = "block";
         message.innerText = `${tamagotchi.name} took a nap!`;
         activities.prepend(message);
 
@@ -248,7 +225,6 @@ class Game {
         const leavingTamas = Game.checkTamagotchis();
         Game.render();
 
-        activityContainer.style.display = "block";
         message.innerText = `You played with ${tamagotchi.name}!`;
         activities.prepend(message);
 
@@ -260,18 +236,16 @@ class Game {
         const leavingTamas = Game.checkTamagotchis();
         Game.render();
 
-        activityContainer.style.display = "block";
         message.innerText = `You fed ${tamagotchi.name}!`;
         activities.prepend(message);
 
         Game.handleLeavingTamas(leavingTamas);
       });
 
-      //Create DOM for timer, if a tamagotchi doesn't have a timer, create Dom element, if it does, reuse it.
+      //If a tamagotchi doesn't have a timer, create DOM element, if it does, reuse it.
       let timerDisplay;
       if (!tamagotchi.timerDisplay) {
         timerDisplay = document.createElement("p");
-        // timerDisplay.classList.add("timer-display");
         timerDisplay.innerText = "10s";
         tamagotchi.timerDisplay = timerDisplay;
       } else {
@@ -282,9 +256,9 @@ class Game {
       btnDiv.append(napBtn, playBtn, eatBtn);
       tamaDiv.append(
         timerDisplay,
-        animalImage,
         name,
         animalType,
+        animalImage,
         energyBar,
         energy,
         fullnessBar,
